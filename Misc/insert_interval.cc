f362a1ae16240156bc7a0c95106f59b98eef0038
Ralph <ralphxwz@gmail.com>
2023-03-29 22:03:50 +0800

    Update insert_interval.cc

--- Misc/insert_interval.cc
+++ Misc/insert_interval.cc
@@ -1,15 +1,17 @@
+/* LC: 57*/
+
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> result;
 
-		// newInterval is the only one to return
+        // newInterval is the only one to return
         if (intervals.size() == 0) {
             result.push_back(newInterval);
             return result;
         }
 
-		// first insert those less than newInterval and not intersect
+        // first insert those less than newInterval and not intersect
         int i;
         for (i = 0; i < intervals.size(); ++i) {
             if (intervals[i][1] < newInterval[0])
@@ -18,7 +20,7 @@ public:
                 break;
         }
 
-		// case1: newInterval is the largest one
+        // case1: newInterval is the largest one
         // newInterval[0] > intervals[size-1][1] 
         if (i == intervals.size()) {
             result.push_back(newInterval);
@@ -36,7 +38,7 @@ public:
         }
 
         // intervals[i][0] <= newInterval[1]
-		// case 3: newInterval intersect interval[i]
+        // case 3: newInterval intersect interval[i]
         vector<int> v{0, 0};
         v[0] = min(intervals[i][0], newInterval[0]);
 
