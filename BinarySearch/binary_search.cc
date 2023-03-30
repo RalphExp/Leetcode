828486004148fad4a9ab007a03edde7a24452332
ralph <ralphxwz@gmail.com>
2023-03-30 13:25:42 +0800

    bug fix

--- BinarySearch/binary_search.cc
+++ BinarySearch/binary_search.cc
@@ -6,13 +6,16 @@ using namespace std;
 
 namespace lc {
 
-/* LC: 34 binary_search returns the first index whose value is 
- * closest to the target from the left, note this is different
- * from the way std::lower_bound does. */
-
+/* LC: 34-35 binary_search returns the first index where 
+ * #1 v[idx] equals target or
+ * v[idx] is the proper place to insert target
+ */
 int binary_search(vector<int> &nums, int target) {
     if (nums.size() == 0)
-        return -1;
+        return 0;
+
+    if (target > nums[nums.size()-1])
+        return nums.size();
 
     size_t left = 0;
     size_t right = nums.size()-1;
@@ -26,12 +29,7 @@ int binary_search(vector<int> &nums, int target) {
             right = mid;
     }
 
-    if (nums[left] <= target)
-        return left;
-
-    // may return -1
-    return left - 1;
-   
+    return left;
 }
 
 } // end namespace
@@ -40,11 +38,13 @@ int main() {
     using namespace lc;            
  
     vector<int> v{1, 2, 3, 6, 7, 8, 9, 13};
+    printf("Search -2 => %d\n", binary_search(v, -2));    
     printf("Search 0 => %d\n", binary_search(v, 0));    
     printf("Search 1 => %d\n", binary_search(v, 1));    
     printf("Search 2 => %d\n", binary_search(v, 2));    
     printf("Search 4 => %d\n", binary_search(v, 4));    
     printf("Search 7 => %d\n", binary_search(v, 7));    
+    printf("Search 8 => %d\n", binary_search(v, 8));    
     printf("Search 13 => %d\n", binary_search(v, 13));    
     printf("Search 14 => %d\n", binary_search(v, 14));    
 }
